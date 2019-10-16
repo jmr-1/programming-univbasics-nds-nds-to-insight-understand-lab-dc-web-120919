9a123ec2b706ede7eb072e4d7e265c5faf661b26
J <jmrumingan@gmail.com>
2019-10-16 19:05:16 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,9 +11,12 @@ end
 def print_first_directors_movie_titles
   
   pp directors_database[0][:movies]
-  spielberg_movies = []
-  
+  spielberg_movies = directors_database[0][:movies]
   movie_counter = 0 
+  
+  while movie_counter<spielberg_movies.length do
+    puts directors_database[0][:movies][movie_counter][:title]
+  end 
   # while movie_counter<directors_database[director].length do
   #   puts "#{nds[:Spielberg][:movie]}"
   #   movie_counter += 1 
